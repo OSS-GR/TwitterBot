9784b99ff7492f0be776ca1715f7d6147f1e4510
Orestis Stefanis <68253122+magas3000@users.noreply.github.com>
2021-04-05 19:26:19 -0400

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,5 +1,20 @@
 #Add your information for your own Twitter Account. You will need a Twitter developer account in order to receive the following keys.
-api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
-api_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-access_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-access_token_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+class authtwit:
+
+     def __init__(self):
+        self.api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
+        self.api_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        self.access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        self.access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+    def get_api_key(self):
+        return self.api_key
+
+    def get_api_secret(self):
+        return self.api_secret
+
+    def get_access_token(self):
+        return self.access_token
+
+    def get_access_token_secret(self):
+        return self.access_token_secret
